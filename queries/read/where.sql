136ff06dde1c6cfbead7002bf0ae0d8f36636961
Shawn Pradeep <shawnprad@hotmail.com>
2023-03-22 13:25:09 -0400

    distinct + where upload

--- queries/read/where.sql
+++ queries/read/where.sql
@@ -2,7 +2,7 @@ USE sql_store;
 
 SELECT *
 FROM products
-WHERE quantity_in_stock > 30
+WHERE quantity_in_stock > 30;
 -- ORDER BY name;
 
 /*
@@ -20,3 +20,19 @@ AND
 OR
 NOT
 */
+
+USE sql_store;
+
+SELECT *
+FROM customers
+WHERE NOT(customer_id > 2 AND customer_id < 9)
+ORDER BY customer_id DESC
+LIMIT 2;
+
+
+SELECT *
+FROM customers
+WHERE last_name like "b%";
+
+-- like allows us to provide a pattern, the above pattern is all last names that start with 'b'
+
